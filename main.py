8411544cf731cf7eadc40f9839eaa8eab7c716fa
Foster-11 <rt.susana@hotmail.com>
2026-04-13 14:18:19 -0500

    creating folders and empty files, configuring main.py

--- main.py
+++ main.py
@@ -1,34 +1,12 @@
-from fastapi import FastAPI
-from datetime import datetime # func timeNow()
-from zoneinfo import ZoneInfo # func timeNow()
+import uvicorn
 
+def start():
+    uvicorn.run(
+        "api.webapi:app",
+        host="127.0.0.1",
+        port=8070,
+        reload=True
+    )
 
-from pydantic import BaseModel
-
-app = FastAPI()
-
-# Para hacer que el metodo sea un endpoint, se utiliza el decorador  (@app.get("/"))
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-# Bussines Rule: Colombian timezone
-country_timezone = {
-    "CO": "America/Bogota",
-}
-
-@app.get("/timeNow/{iso_code}")
-async def time(iso_code: str):
-    iso = iso_code.upper()
-    timezone_str = country_timezone.get(iso)
-    tz = zoneinfo.ZoneInfo(timezone_str)
-    return{"time": datetime.now(tz)}
-
-
-# model
-
-class Customer(BaseModel):
-    name = str
-    description: str | None # pipe operator ( "|" ) allows to set the type as optional (None) or str
-    
+if __name__ == "__main__":  
+    start()
