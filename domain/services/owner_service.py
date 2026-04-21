a39a128cb2adfea97542f3137dfc668091b4a8f7
Susana Ríos Tabares <133529784+Foster-11@users.noreply.github.com>
2026-04-21 11:27:30 -0500

    Update owner_service.py

--- domain/services/owner_service.py
+++ domain/services/owner_service.py
@@ -1,5 +1,5 @@
 # domain/ : Razon de ser del sistema, nucleo del negocio testeable
-# ../services/: logica real,  reglas del negocio, toma de decisiones.
+# ../services/: logica real,  reglas del negocio, lanzamiento de excepciones (fail fast + graceful handling)
 #   inputs:
 #       datos primitivos
 #       repositorios/sesiones (inyectadas)
@@ -14,10 +14,22 @@
 
 # Respondabilidades clave:
 # 1. Registrar owner - pending
-# 2. Validar email - pending
+# 2. Validar email para login - pending
 # 3. hashear contraseña - pending
-# 4. validar credenciales - pending
+# 4. validar credenciales para login - pending
 # 5. login : email existe y contraseña coincide - pending
 
 from domain.models.owner import Owner
+from domain.exceptions import OwnerAlreadyExistsError, InvalidCredentialsError
+from domain.security import hash_password, verify_password
 
+def register_owner(
+  *,
+  owner_repo,
+  name: str,
+  last_name: str,
+  email: str,
+  password: str
+) -> Owner:# Qué es? por qué esta funcion es diferente a la de los servicios de las demás entidads?
+  existing_owner
+  
