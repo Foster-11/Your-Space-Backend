b1a28aa7763910cab328e43916e6e799bda3296b
Foster-11 <rt.susana@hotmail.com>
2026-04-27 22:17:34 -0500

    Configure fetures for testing in conftest.py and create the firts unit test on main.py

---
+++ test/unit/test_main.py
@@ -0,0 +1,27 @@
+# Testear: 
+# 1. uvicorn.run sea invocado
+# 2. con qué argumentos
+# 3. que start() tenga un comportamiento esperado, o sea, llamar a uvicorn.run
+
+#test_main.py
+import main
+from unittest.mock import patch
+import uvicorn
+
+def test_start_calls_uvicorn_run_with_correct_arguments():
+    #arrange
+    with patch.object(uvicorn, "run") as mock_run:# no arranca un servidor real
+        # un mock es un objeto simulado que imita el comportamiento real de un componente
+        
+        # act
+        main.start()
+
+        # assert
+        mock_run.assert_called_once_with(
+            "api.app:app",
+            host="127.0.0.1",
+            port=8070,
+            reload=True
+        )
+
+         
--- test/unit/test_space_service.py
+++
@@ -1,4 +0,0 @@
-from domain.services.space_service import create_space
-
-def test_create_space_default() -> None:
-    create_space = create_space() 
